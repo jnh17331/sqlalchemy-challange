fa68eff177d03b6bbba463ee3010aef8ec2a3a01
jnh17331 <jnh17331@gmail.com>
2023-06-27 21:17:55 -0400

    Cleaned and commented code

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,4 +1,4 @@
-# Import the dependencies.
+# Dependencies
 import sqlalchemy
 from flask import Flask, jsonify
 import datetime as dt
@@ -12,25 +12,21 @@ from sqlalchemy import create_engine, func, inspect
 #################################################
 
 
-# reflect an existing database into a new model
-
+# Creates an engine for the sql file
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
+# Sets up our base
 Base = automap_base()
-
 Base.prepare(engine, reflect=True)
 
-# reflect the tables
-
+# Reflect the tables
 Base.classes.keys()
 
 # Save references to each table
-
 Measurement  = Base.classes.measurement
 Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
 session = Session(engine)
 
 #################################################
@@ -42,6 +38,7 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
 
 @app.route("/")
 def main():
@@ -57,68 +54,94 @@ def main():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-        
+
+    # Gets the range of days in the previous year    
     prev_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+
+    # Queries the data to get precipitation measurements for the last year
     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= prev_year).order_by(Measurement.date).all()
 
+    # Puts the results of the query into a dictonary
     results_dict = dict(results)
+
+    # Closes the session
     session.close()
+
+    # Returns the dictionary as a json
     return jsonify(results_dict)
 
 
 @app.route("/api/v1.0/stations")
 def stations():
-    stations = session.query(Measurement.station, func.count(Measurement.id)).\
-            group_by(Measurement.station).order_by(func.count(Measurement.id).desc()).all()
 
+    # Queries the stations from the data; then groups and orders the stations by id
+    stations = session.query(Measurement.station, func.count(Measurement.id)).group_by(Measurement.station).order_by(func.count(Measurement.id).desc()).all()
+
+    # Puts the results of the query into a dictonary
     stations_dict = dict(stations)
+
+    # Closes the session
     session.close()
+
+    # Returns the dictionary as a json
     return jsonify(stations_dict)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    max_temp_obs = session.query(Measurement.station, Measurement.tobs).\
-        filter(Measurement.date >= '2016-08-23').all()
 
+    # Queries the data for the station and observed temp and filters the queried data for dates from 8-23-2016 and above
+    max_temp_obs = session.query(Measurement.station, Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= '2016-08-23').all()
+
+    # Puts the results of the query into a dictonary
     tobs_dict = dict(max_temp_obs)
+
+    # Closes the session
     session.close()
+
+    # Returns the dictionary as a json
     return jsonify(tobs_dict)
 
 @app.route("/api/v1.0/<start>")
 def start(start):
-    result = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),\
-                                func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
+
+    # Gets the min value, mean value, and max value of an observed temp from a specified date from the sql data
+    result = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
     
-    session.close()
+    # Creates an empty list for the observed temp results
     tobsall = []
 
-    for min,avg,max in result:
+    # Loops through the query and gets the min, avg, and max value and appends it to the empty list
+    for min, avg, max in result:
         tobs_dict = {}
         tobs_dict["Min"] = min
         tobs_dict["Average"] = avg
         tobs_dict["Max"] = max
         tobsall.append(tobs_dict)
         
+    # Closes the session
+    session.close()
+
+    # Returns the list of the queried data
     return jsonify(tobsall)
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start, end):
     
+    # Gets the start and end date and puts them as a date in datetime
     start_date = dt.datetime.strptime(start, "%Y-%m-%d").date()
     end_date = dt.datetime.strptime(end, "%Y-%m-%d").date()
 
-    
+    # Queries the sql data for the min, avg, and max temp of the start and end date
     result = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
 
-    session.close()
-
-    
+    # Tries to get the data if there are valid dates, if not; it returns an error message
     if result:
-        # Create a list to store the temperature data
+ 
+        # Creates an empty list for the data
         tobsall = []
 
-        
+        # Loops through the queried data and appends it to the empty list
         for min_temp, avg_temp, max_temp in result:
             tobs_dict = {}
             tobs_dict["Min"] = min_temp
@@ -126,9 +149,13 @@ def start_end(start, end):
             tobs_dict["Max"] = max_temp
             tobsall.append(tobs_dict)
 
+        # Closes the session
+        session.close()
+
+        # Returns the list
         return jsonify(tobsall)
     else:
-        
+
         return jsonify({"message": "No data found for the given date range."})
 
 
